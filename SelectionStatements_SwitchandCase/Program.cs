1802a93dfcee723eb7a3fefdb3f1a6f8ebd6db1f
aburbank <ashlynburbank@gmail.com>
2020-08-25 14:34:18 -0500

    Finished Switch/Case exercise 2

---
+++ SelectionStatements_SwitchandCase/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SelectionStatements_SwitchandCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey! What is your favorite subject in school?");
+            var favSchoolSub = Console.ReadLine();
+
+            switch (favSchoolSub.ToLower())
+            {
+                case "history":
+                    Console.WriteLine("History is a great subject!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is a fun thing to learn!");
+                    break;
+                default:
+                    Console.WriteLine("I don't know of that subject... I just like history and science!");
+                    break;
+            }
+        }
+    }
+}
